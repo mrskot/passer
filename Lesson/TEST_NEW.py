900d98a6b83058cfe4a7f0ce40c681363404a6a1
mrskot <mr.pokazanev@gmail.com>
2018-01-25 17:29:04 +0300

    Добавил фильтры спецификаций 04.0..

--- Lesson/TEST_NEW.py
+++ Lesson/TEST_NEW.py
@@ -33,7 +33,8 @@ def read_file(file_name):
 def filter_spec(spec_list, file_name):
     spec_list = spec_list
     file_name = file_name
-    filter_name = [file_name, '', '', '', '', '']
+    filter_name = [file_name, None, None, None, None, None, None, None, None,
+                   None, None, None, None, None, None, None, None, None, None, None]
 
     for spec in spec_list:
         if spec[0:3] in '02.0':
@@ -41,17 +42,48 @@ def filter_spec(spec_list, file_name):
             korpus_name = filter_korpusa(korpus_number)
 
             filter_name[1] = korpus_name
-            #break
-#        elif spec[0:8] in 'Шкаф ШПТ':
-#            korpus_name = spec[5:12]
-#            filter_name[1] = korpus_name
 
-#        elif spec[0:3] in '04.0':
-#            detal_1_number = spec[0:12]
-#            detal_1_name = filter_detal_1(detal_1_number)
-#
-#            filter_name[2] = detal_1_name
-            #break
+        elif spec[0:8] in 'Шкаф ШПТ':
+            korpus_name = spec[5:12]
+            filter_name[1] = korpus_name
+
+
+
+        elif spec[0:6] in '04.001':
+            filter_name[8] = spec
+
+        elif spec[0:6] in '04.004':
+            filter_name[9] = spec
+
+        elif spec[0:6] in '04.005':
+            filter_name[10] = spec
+
+        elif spec[0:6] in '04.008':
+            filter_name[11] = spec
+
+        elif spec[0:6] in '04.009':
+            filter_name[12] = spec
+
+        elif spec[0:6] in '04.010':
+            filter_name[13] = spec
+
+        elif spec[0:6] in '04.011':
+            filter_name[14] = spec
+
+        elif spec[0:6] in '04.014':
+            filter_name[15] = spec
+
+        elif spec[0:6] in '04.015':
+            filter_name[16] = spec
+
+        elif spec[0:6] in '04.016':
+            filter_name[17] = spec
+
+        elif spec[0:6] in '04.018':
+            filter_name[18] = spec
+
+        elif spec[0:6] in '04.022':
+            filter_name[19] = spec
 
     return filter_name
 
@@ -63,25 +95,27 @@ def filter_korpusa(korpus_number):
 
     tip = int(korpus_number[4])
     num = int(korpus_number[7])
-    #korpus_name = korpus_number
+
     korpus_name = str(korpus_tip[tip - 1]) + '.' + str(korpus_num[num - 1])
 
     return korpus_name
 
-def filter_detal_1(detal_1_number):
-    detal_1_number = detal_1_number
-    detal_1_name = 'detal_1_number' + detal_1_number
-    return detal_1_name
-
-
-
 # функция получает список столбцов и добавляет их в базу данных
 def add_spec(filter_list):
     add_list = filter_list
-    db = MySQLdb.connect("localhost", "mrskot", "mrskot", "for_pyqt", charset='utf8', init_command='SET NAMES UTF8')
+    db = MySQLdb.connect("localhost", "mrskot", "mrskot", "for_pyqt",
+                         charset='utf8', init_command='SET NAMES UTF8')
     cursor = db.cursor()
     cursor.execute("SELECT * FROM test")
-    sql = "INSERT INTO test(PROJECT, TIP_KORPUSA, USTANOVKA) VALUES ('%s','%s','%s')" % (add_list[0], add_list[1], add_list[2])
+    sql = "INSERT INTO test(PROJECT, TIP_KORPUSA, OBOGREV, ADAPTOR_TRUBNIY, OSNOLAYN_KVS, KAPILYARI," \
+          " PILT, TERMOSTAT, 04_001_ADAPTOR,04_004_PLITA, 04_005_PLANKA, 04_008_STOYKA, 04_009_USEL_NA_BOB," \
+          " 04_010_USEL_NA_TRUB, 04_011_USEL_NA_FLAN, 04_014_KRONSHTEYN_NA_POV, 04_015_USTROYSTVO_KAPILAR," \
+          " 04_016_STEKLOPAKET, 04_018_OPORA_MONTAJNAYA, 04_022_KRONSHTEYNI) " \
+          "VALUES ('%s','%s','%s','%s','%s','%s', '%s','%s','%s','%s'," \
+          "'%s','%s','%s','%s', '%s','%s','%s','%s','%s','%s')" \
+          % (add_list[0], add_list[1], add_list[2], add_list[3], add_list[4], add_list[5], add_list[6],
+             add_list[7], add_list[8], add_list[9], add_list[10], add_list[11], add_list[12], add_list[13],
+             add_list[14], add_list[15], add_list[16], add_list[17], add_list[18], add_list[19])
     try:
         cursor.execute(sql)
         db.commit()
